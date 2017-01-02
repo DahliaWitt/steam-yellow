496fa41e1a640b67bbee5f4102e8fd5391ba9a58
Patrick Kubiak <patrick@antuple.net>
2017-01-02 16:45:30 -0500

    console logging support

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+// yellow: node index.js [username] [password] 4
+
 const SteamUser = require('steam-user');
 const client = new SteamUser();
 
 SteamUser.prototype.setPersona = function(state, name) {
+	console.log(getTime() + " ID: " + client.steamID.getSteam3RenderedID() +
+							" State: " + state + " Flags: " + process.argv[4]);
+
 	this._send(SteamUser.EMsg.ClientChangeStatus, {
-		"persona_state": state,
-		"persona_state_flags": 4,
+		"persona_state": state,	// state
+		"persona_state_flags": parseInt(process.argv[4]),	// flags
 		"player_name": name
 	});
 };
 
 const logOnOptions = {
-    accountName: process.argv[2],
-    password: process.argv[3]
+	accountName: process.argv[2],
+	password: process.argv[3]
 };
 
 client.logOn(logOnOptions);
 
 client.on('loggedOn', () => {
-    client.setPersona(SteamUser.Steam.EPersonaState.Online);
-    console.log("Logged In! Press CTRL and C to stop.");
+	client.setPersona(SteamUser.Steam.EPersonaState.Online);
+	console.log(getTime() + " Logged In! Press CTRL and C to stop.");
 });
+
+function getTime() {
+	var time = new Date();
+	return "[" + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "]";
+}
