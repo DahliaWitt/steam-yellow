e5bfd41d9cd3f3fc67b2c5d7fdffb0d1acc2b9ec
Drake Witt <dwitt@dranweb.com>
2017-01-03 02:12:02 -0500

    Make batch files great again

--- start.bat
+++ start.bat
@@ -1,4 +1,40 @@
 @echo off
 title /!\Steam Gold/!\
 color E
+echo 
+echo                                                                                                     
+echo                                                                                                     
+echo                                                                              .-:/+ooo+/:.`          
+echo                                                                          `:ohmNMMMMMMMMMNmy+-       
+echo                                                                        .odMMMNdys++++oshmNMMNh/`    
+echo                                                                      `+mMMNh/.`         `-odMMMd:   
+echo                                                                     .hMMMy-   .:+syyys/-`  `/mMMNo  
+echo                                                                    `dMMN/   -ymMMMMMMMMNd+`  .yMMMo 
+echo                                                                    sMMM/   oNMMMMMMMMMMMMMd-  `hMMM:
+echo           ``````                                                  .NMMy   +MMMMMMMMMMMMMMMMm`  .MMMh
+echo      `./shdmmmmdhs/.`                                             :MMM/   mMMMMMMMMMMMMMMMMM/   mMMN
+echo    `/hNMMMNNNNNNMMMNh/`                                           oMMM/   mMMMMMMMMMMMMMMMMM/   mMMN
+echo   :dMMMNy/-.```.:odMMMm:                                         .NMMMy   +MMMMMMMMMMMMMMMMm`  .MMMh
+echo  +MMMNo.   ./osso:./dMMMo                                        hMMMMM/   oNMMMMMMMMMMMMMm-  `hMMM-
+echo :MMMN-   :hMMMMMMMMmdMMMM/                                      /MMMMMMN/   -yNMMMMMMMMMm+`  .hMMM+ 
+echo dMMM+   +MMMMMMMMMMMMMMMMNhs/-`                                `NMMMMMMMMh-   `:oyyhys+-   `+NMMN+  
+echo MMMM-  `MMMMMMMMMMMMMMMMMMMMMMMmho/-`                          yMMMMMMMMMMMd+.          `:sNMMMh.   
+echo mMMM/  `MMMMMMMMMMMMMMMMMMMMMMMMMMMMMmyo/.                .:/+sMMMMMMMMMMMMMMMNhsoo+osydMMMMNy-     
+echo +MMMm.  sMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdy+:.      .+hNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMds:`       
+echo  yMMMm/  /mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNds+/hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmo.           
+echo   oNMMMd+-`-+ydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMdyyhmNMMMMMMMMMMMMMMMMMMMd/`             
+echo    .sNMMMNNmddmNMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNmhs+-:sNMMMMMMMMMMMMMNy:                
+echo      `/sdNMMMMMMNds/``./oydNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNh-.sMMMMMMMMMMms.                  
+echo         ``.----.``        ``-/shdNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN- +MMMMMMMm+`                    
+echo                                 `.-+shmNMMMMMMMMMMMMMMMMMMMMMMMMMMMy  hMMMMMMh                      
+echo                                       `.:+yhmNMMMMMMMMMMMMMMMMMMMMMy  +MMMMMMh                      
+echo                                            ``.:ohMMMMMMMMMMMMMMMMMm.  sMMMMMM+                      
+echo                                                 `dMh/oydmMMMMMMMms.  -NMMMMMm`                      
+echo                                                  .mMs` `.-+oss+:.   /mMMMMMm-                       
+echo                                                   .yNmo-`        ./hMMMMMMy.                        
+echo                                                     :hNNdyo///+shmMMMMMNh:`                         
+echo                                                       -+hmNMMMMMMMMNmho-                            
+echo                                                          `-:+oooo/:-`                               
+echo                                                                                                   
+echo 
 node index.js
